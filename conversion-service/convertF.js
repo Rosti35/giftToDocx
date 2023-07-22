79bff3ce54a5f1b51ea2bc6391a8d75ab6c1cd8d
Rostislav Shepel <efyrs5@exmail.nottingham.ac.uk>
2023-07-23 02:45:46 +0300

    new convertation method

--- conversion-service/convertF.js
+++ conversion-service/convertF.js
@@ -1,68 +1,92 @@
 const fs = require('fs');
 const path = require('path');
-const officegen = require('officegen');
+const PizZip = require('pizzip');
+const Docxtemplater = require('docxtemplater');
 
 function convertFile(file) {
   return new Promise((resolve, reject) => {
     fs.readFile(file.path, 'utf8', (err, data) => {
       if (err) {
-        console.log('Error reading file:', err); // Log error
+        console.error('Error reading file:', err); // Log error
         reject('Error reading file');
         return;
       }
 
-      // Perform transformations
-      let giftContent = data;
-      giftContent = giftContent.replace(/{/g, "").replace(/}/g, "").replace(/~/g, "WRONG: ").replace(/=/g, "ANSWER: ");
-      giftContent = giftContent.replace(/%(.*)%/g, "(points: $1) ");
+      let giftContent = data
+        .replace(/{/g, "")
+        .replace(/}/g, "")
+        .replace(/~/g, "")
+        .replace(/=/g, "")
+        .split("\n");
 
-      const docx = officegen('docx');
-      docx.on('error', function (err) {
-        console.log('Error generating docx:', err); // Log error
-      });
+      let questionNumber = 1;
+      let question = "";
+      let answers = [];
+      let table = [];
 
-      const pObj = docx.createP();
-
-      const lines = giftContent.split("\n");
-      for (const line of lines) {
-        if (line.trim().startsWith('//')) {
-          pObj.addText(line, { italic: true }); // Add comment in italic
-        } else {
-          const parts = line.split('#');
-          pObj.addText(parts[0]);
-          if (parts.length > 1) {
-            pObj.addText(' #' + parts[1], { color: '888888' }); // Add comment in grey color
+      giftContent.forEach((line, index) => {
+        try {
+          line = line.trim();
+          if (line.startsWith("::")) {
+            question = line.split("::")[2].split("[html]<p>")[1].split("</p>")[0];
+          } else if (line.startsWith("<p>")) {
+            answers.push(line.replace("<p>", "").replace("</p>", ""));
+          } else if (line === "") {
+            if (answers.length > 0) {
+              table.push({ '№': questionNumber, 'Question': question, 'Answers': answers.join(", ") });
+              questionNumber++;
+              question = "";
+              answers = [];
+            }
           }
+        } catch (error) {
+          console.error(`Error processing line ${index + 1}: ${line}`, error);
         }
-        pObj.addLineBreak();
-      }
-      const dirPath = path.join(__dirname, 'public');
-      const outputPath = path.join(dirPath, `${file.filename}.docx`);
-      
+      });
+
+      const dirPath = path.join(__dirname);
+      const templatePath = path.join(dirPath, 'template', 'template.docx');
+      const outputPath = path.join(dirPath, 'public', `${file.filename}.docx`);
+
       // Create the directory if it does not exist
-      fs.mkdirSync(dirPath, { recursive: true });
-      
-      const out = fs.createWriteStream(outputPath);
-      out.on('error', function (err) {
-        console.log('Error writing docx file:', err); // Log error
-        reject('Error writing docx file');
+      fs.mkdirSync(path.join(dirPath, 'public'), { recursive: true });
+
+      // Load the docx file as a binary
+      const content = fs.readFileSync(templatePath, 'binary');
+
+      const zip = new PizZip(content);
+
+      const doc = new Docxtemplater();
+      doc.loadZip(zip);
+
+      // set the templateVariables
+      doc.setData({ table: table });
+
+      try {
+        // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
+        doc.render();
+      } catch (error) {
+        console.error('Error generating docx:', error); // Log error
+        reject('Error generating docx');
+      }
+
+      const buf = doc.getZip()
+        .generate({ type: 'nodebuffer' });
+
+      // write the generated docx to a file
+      fs.writeFileSync(outputPath, buf);
+
+      console.log('Finished writing docx file:', outputPath); // Log
+
+      fs.unlink(file.path, (err) => {
+        if (err) {
+          console.error('Failed to delete original file:', err); // Log error
+        } else {
+          console.log('Deleted original file:', file.path); // Log success
+        }
       });
-      
-      docx.generate(out);
 
-      out.on('finish', function () {
-        console.log('Finished writing docx file:', outputPath); // Log
-
-        fs.unlink(file.path, (err) => {
-          if (err) {
-            console.log('Failed to delete original file:', err); // Log error
-          } else {
-            console.log('Deleted original file:', file.path); // Log success
-          }
-        });
-
-        resolve(`${file.filename}.docx`);
-      });
+      resolve(`${file.filename}.docx`);
     });
   });
 }
